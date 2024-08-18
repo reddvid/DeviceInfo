09107db6f41bde70e053b30f14abcd3784e831bb
David Ballesteros <david_ballesteros@outlook.com>
2024-08-19 01:41:43 +0800

    Check args

--- DeviceInfo.Console/Program.cs
+++ DeviceInfo.Console/Program.cs
@@ -2,27 +2,36 @@
 using DeviceInfo.Console;
 using Hardware.Info;
 
-var dataArg = args[1];
+if (args.Length != 0)
+{
+    var dataArg = args[1];
 
-Console.WriteLine($"Getting info for {args[0]} PC");
+    Console.WriteLine($"Getting info for {args[0]} PC");
 
-if (dataArg is "--all" or "-a" or "" or ".")
+    if (dataArg is "--all" or "-a" or "" or ".")
+    {
+        Console.WriteLine("Getting all info.");
+
+        await GetDeviceInfo(args);
+
+        await GetAppList(args);
+    }
+    else if (dataArg is "--system" or "-s" or "--devices" or "-d")
+    {
+        Console.WriteLine("Getting device hardware info.");
+
+        await GetDeviceInfo(args);
+    }
+    else if (dataArg is "--apps" or "-p")
+    {
+        Console.WriteLine("Getting app list.");
+
+        await GetAppList(args);
+    }
+}
+else
 {
     Console.WriteLine("Getting all info.");
 
     await GetDeviceInfo(args);
-
-    await GetAppList(args);
-}
-else if (dataArg is "--system" or "-s" or "--devices" or "-d")
-{
-    Console.WriteLine("Getting device hardware info.");
-
-    await GetDeviceInfo(args);
-}
-else if (dataArg is "--apps" or "-p")
-{
-    Console.WriteLine("Getting app list.");
-
-    await GetAppList(args);
 }
